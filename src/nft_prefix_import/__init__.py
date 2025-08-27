066d0e269bcdba9a118d5447a5372a207e066ec3
Martin Weinelt <hexa@darmstadt.ccc.de>
2025-08-28 01:19:36 +0200

    Use a single call per AFI
    This scales until the max length for a command line, but that is likely
    sufficient for now. Ideally we could use pyroute2, but in version 0.9.4
    the nftables set interface is broken.

--- src/nft_prefix_import/__init__.py
+++ src/nft_prefix_import/__init__.py
@@ -3,10 +3,12 @@ import time
 from ipaddress import ip_network
 from pathlib import Path
 from subprocess import CalledProcessError, run
+from traceback import print_exc
 from typing import Final
 
 import httpx
 import typer
+from more_itertools import partition
 from typing_extensions import Annotated
 
 
@@ -18,10 +20,10 @@ def get_rttable(user_agent: str) -> str:
         assert response.status_code == 200
         return response.text
 
-    RTTABLE_CACHE: Final = Path("./table.txt")
+    rttable_cache: Final = Path("./table.txt")
 
     try:
-        mtime = RTTABLE_CACHE.stat().st_mtime
+        mtime = rttable_cache.stat().st_mtime
     except FileNotFoundError:
         mtime = None
 
@@ -29,45 +31,60 @@ def get_rttable(user_agent: str) -> str:
     if not mtime or time.time() - mtime > 2 * 3600:
         print("Pulling routing table from bgp.tools", file=sys.stderr)
         data = fetch()
-        with RTTABLE_CACHE.open("w") as fd:
+        with rttable_cache.open("w") as fd:
             fd.write(data)
     else:
         print("Loading routing table from cache", file=sys.stderr)
-        with RTTABLE_CACHE.open() as fd:
+        with rttable_cache.open() as fd:
             data = fd.read()
 
     return data
 
 
-def nft_block(prefixes: set[str], table: str, ipv4set: str, ipv6set: str) -> None:
+def nft_add_elements(
+    prefixes: set[str], table: str, ipv4set: str, ipv6set: str
+) -> None:
     networks = map(ip_network, prefixes)
+    ipv4nets, ipv6nets = partition(lambda ip: ip.version == 6, networks)
 
-    for network in networks:
-        try:
-            run(
-                [
-                    "nft",
-                    "add",
-                    "element",
-                    "inet",
-                    table,
-                    ipv4set if network.version == 4 else ipv6set,
-                    "{",
-                    str(network),
-                    "}",
-                ],
-                check=True,
-            )
-        except CalledProcessError:
-            continue
+    def add_elements(setname: str, elements: list[str]) -> None:
+        run(
+            [
+                "nft",
+                "add",
+                "element",
+                "inet",
+                table,
+                setname,
+                "{",
+                ", ".join(map(str, elements)),
+                "}",
+            ],
+            check=True
+        )
+    try:
+        add_elements(ipv4set, ipv4nets)
+    except CalledProcessError:
+        print_exc()
+
+    try:
+        add_elements(ipv6set, ipv6nets)
+    except CalledProcessError:
+        print_exc()
 
 
 def main(
-    autnums: Annotated[list[str], typer.Argument(help="List of autonomous systems (AS) numbers")],
+    autnums: Annotated[
+        list[str], typer.Argument(help="List of autonomous systems (AS) numbers")
+    ],
     user_agent: Annotated[str, typer.Option(envvar="USER_AGENT")],
     table: Annotated[str, typer.Option(help="Table in nftables to target")] = "filter",
-    ipv4set: Annotated[str, typer.Option(help="Set for IPv4 prefixes")] = "ipv4prefixes",
-    ipv6set: Annotated[str, typer.Option(help="Set for IPv6 prefixes")] = "ipv6prefixes",
+    ipv4set: Annotated[
+        str, typer.Option(help="Set for IPv4 prefixes")
+    ] = "ipv4prefixes",
+    ipv6set: Annotated[
+        str, typer.Option(help="Set for IPv6 prefixes")
+    ] = "ipv6prefixes",
 ) -> None:
     rttable = get_rttable(user_agent)
 
@@ -79,4 +96,4 @@ def main(
             continue
         if autnum in autnums:
             prefixes.add(prefix)
-    nft_block(prefixes, table, ipv4set, ipv6set)
+    nft_add_elements(prefixes, table, ipv4set, ipv6set)
